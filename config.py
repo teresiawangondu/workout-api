8abb71ba970b5e1c7a9490c6433e4ea13ad27c49
teresiawangondu <arwatess@gmail.com>
2026-08-03 02:31:41 +0300

    Add configuration comment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
+# Application configuration settings
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
         BASE_DIR,
